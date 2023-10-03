fd433862a6c15c9f503b004cc1a3b3c904a74c79
Deadly Artist <44524817+DeadlyArtist@users.noreply.github.com>
2023-10-03 18:17:02 +0200

    Create TruePixelPerfect.cs

---
+++ TruePixelPerfect.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
+[AddComponentMenu("Rendering/2D/True Pixel Perfect")]
+[RequireComponent(typeof(Camera))]
+public class TruePixelPerfect : MonoBehaviour
+{
+	public int spritePixelsPerUnit = 32;
+	public int maxVerticalSpritePixels = 256;
+
+	private int pixelsPerSpritePixel = 4;
+	private float unevenPixelHeightOffset = 0;
+	private float unevenPixelWidthOffset = 0;
+	private Rect pixelRect;
+
+	private Camera r_camera;
+
+    void Awake()
+    {
+		r_camera = GetComponent<Camera>();
+		UpdateCamera();
+	}
+
+	void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
+	{
+		if (camera == r_camera)
+		{
+			UpdateCamera();
+		}
+	}
+
+	void OnEnable()
+	{
+		RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
+	}
+
+	void OnDisable()
+	{
+		RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
+
+		transform.position -= new Vector3(unevenPixelWidthOffset, unevenPixelHeightOffset, 0);
+		unevenPixelHeightOffset = 0;
+		unevenPixelWidthOffset = 0;
+
+		r_camera.rect = new Rect(0, 0, 1, 1);
+		r_camera.ResetWorldToCameraMatrix();
+	}
+
+	void UpdateCamera()
+    {
+		var pixelRect = r_camera.pixelRect;
+		this.pixelRect = pixelRect;
+
+		int pixelsPerSpritePixel = (int)Mathf.Ceil(pixelRect.height / maxVerticalSpritePixels);
+		this.pixelsPerSpritePixel = pixelsPerSpritePixel;
+
+		float ortographicSize = pixelRect.height / (pixelsPerSpritePixel * spritePixelsPerUnit);
+		r_camera.orthographicSize = ortographicSize / 2;
+
+		transform.position -= new Vector3(unevenPixelWidthOffset, unevenPixelHeightOffset, 0);
+		if (pixelRect.height % 2 != 0)
+		{
+			float unevenPixelHeightOffset = 1f / (pixelsPerSpritePixel * spritePixelsPerUnit * 2);
+			this.unevenPixelHeightOffset = unevenPixelHeightOffset;
+
+			transform.position += new Vector3(0, unevenPixelHeightOffset, 0);
+		}
+		else
+		{
+			this.unevenPixelHeightOffset = 0;
+		}
+		if (pixelRect.width % 2 != 0)
+		{
+			float unevenPixelWidthOffset = 1f / (pixelsPerSpritePixel * spritePixelsPerUnit * 2);
+			this.unevenPixelWidthOffset = unevenPixelWidthOffset;
+
+			transform.position += new Vector3(unevenPixelWidthOffset, 0, 0);
+		}
+		else
+		{
+			this.unevenPixelWidthOffset = 0;
+		}
+	}
+}
